b04cecc0aa65713d3c0da709ec977f47441bc55e
Ahmed Roshdi <81536637+AhmedRoshdi36@users.noreply.github.com>
2025-08-05 18:49:03 +0300

    Update Merge-Two-Sorted-Lists.cs

--- Merge-Two-Sorted-Lists.cs
+++ Merge-Two-Sorted-Lists.cs
@@ -11,32 +11,25 @@
  */
 public class Solution {
     public ListNode MergeTwoLists(ListNode list1, ListNode list2) 
-     {
-        List<int> values = new List<int>();
-
-        // Step 1: Extract all values from both lists
-        while (list1 != null) {
-            values.Add(list1.val);
-            list1 = list1.next;
-        }
-
-        while (list2 != null) {
-            values.Add(list2.val);
-            list2 = list2.next;
-        }
-
-        // Step 2: Sort the values
-        values.Sort();
-
-        // Step 3: Build a new linked list from sorted values
-        ListNode dummy = new ListNode(-1);
+    {
+        ListNode dummy = new(-1);
         ListNode current = dummy;
 
-        foreach (int val in values) {
-            current.next = new ListNode(val);
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+              current.next = list1;
+              list1 = list1.next;
+            }
+            else
+            {
+                current.next = list2;
+                list2 = list2.next;
+            }
             current = current.next;
         }
-
+        current.next =list1 ?? list2;
         return dummy.next;
 
         
